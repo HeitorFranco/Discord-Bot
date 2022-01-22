2870428c1a0da758a575127dc884989519841740
Heitor Franco <heitorfranco04@gmail.com>
2022-01-22 16:10:23 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import discum
+import os
+import time
+import discord
+from dotenv import load_dotenv
+
+config = load_dotenv(".env")
+
+token = os.environ['TOKEN']
+serverId = os.environ['SERVER_ID']
+channelId = os.environ['CHANNEL_ID']
+delay = os.environ['DELAY']
+
+bot = discum.Client(token)
+
+print("Start")
+
+def close_after_fetching(resp, guild_id):
+    if bot.gateway.finishedMemberFetching(guild_id):
+        lenmembersfetched = len(bot.gateway.session.guild(guild_id).members)
+        print(str(lenmembersfetched)+' members fetched')
+        bot.gateway.removeCommand({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
+        bot.gateway.close()
+
+def get_members(guild_id, channel_id):
+    bot.gateway.fetchMembers(guild_id, channel_id, keep="all", wait=1)
+    bot.gateway.command({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
+    bot.gateway.run()
+    bot.gateway.resetSession()
+    return bot.gateway.session.guild(guild_id).members
+
+members = get_members(serverId, channelId)
+memberslist = []
+
+for memberID in members:
+    memberslist.append(memberID)
+    print(memberID)
+
+f = open('users.txt', "a")
+for element in memberslist:
+    f.write(element + '\n')
+f.close()
+
+
+
+
+
+client = discord.Client()
+
+ids = open('users.txt', 'r')
+
+print("Start Invites")
+
+@client.event
+async def on_connect():
+    for id in ids:
+        try:
+            time.sleep(delay)
+            user = await client.fetch_user(id)
+
+            await user.send('f"send DM to: {id}"')
+            print(f"send DM to: {id}")
+
+        except ValueError:
+            print(f"couldnt message: {user.name}, {ValueError} ")
+
+
+client.run(token, bot=False)
